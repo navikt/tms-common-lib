d38b280bdaf4cd3f77ac83b6530152c2f87262e6
Christopher Santana <christopher.santana@nav.no>
2024-02-12 15:19:49 +0100

    Fikser teit feil.

--- kubernetes/src/main/kotlin/no/nav/tms/common/kubernetes/PodLeaderElection.kt
+++ kubernetes/src/main/kotlin/no/nav/tms/common/kubernetes/PodLeaderElection.kt
@@ -57,7 +57,7 @@ class PodLeaderElection(
 
         private fun getElectionUrl() = when (val path = System.getenv("ELECTOR_PATH")) {
             null -> throw RuntimeException("Fant ikke variabel ELECTOR_PATH")
-            else -> path
+            else -> "http://$path"
         }
     }
 }
