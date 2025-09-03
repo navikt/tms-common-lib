050f678b7626b124b75fab33945308a73a24369c
rannveigskjerve <55136985+rannveigskjerve@users.noreply.github.com>
2025-09-03 14:38:17 +0200

    Sticky route fix (#15)

--- observability/src/main/kotlin/no/nav/tms/common/observability/Tracelogging.kt
+++ observability/src/main/kotlin/no/nav/tms/common/observability/Tracelogging.kt
@@ -3,6 +3,7 @@ package no.nav.tms.common.observability
 import io.github.oshai.kotlinlogging.withLoggingContext
 import io.ktor.serialization.*
 import io.ktor.server.application.*
+import io.ktor.server.application.hooks.ResponseSent
 import io.ktor.server.request.*
 import io.ktor.util.*
 import org.slf4j.MDC
@@ -69,23 +70,19 @@ suspend fun withApiTracing(
 ) {
     withLoggingContext(
         restorePrevious = false,
-        map= mapOf(
+        map = mapOf(
             "route" to "$method – $route",
             "contenttype" to contenttype.name
         ) + extra
     ) { function() }
 }
 
-class ApiMdc{
-    companion object Plugin : BaseApplicationPlugin<ApplicationCallPipeline, Configuration, ApiMdc> {
 
-        override val key = AttributeKey<ApiMdc>("ApiMdc")
-        override fun install(pipeline: ApplicationCallPipeline, configure: Configuration.() -> Unit): ApiMdc {
-            val plugin = ApiMdc()
-            pipeline.intercept(ApplicationCallPipeline.Monitoring) {
-                MDC.put("route", call.request.uri)
-            }
-            return plugin
-        }
+val ApiMdc = createApplicationPlugin(name = "ApiMdc2") {
+    onCall { call ->
+        MDC.put("route", call.request.uri)
+    }
+    on(ResponseSent) {
+        MDC.remove("route")
     }
 }
