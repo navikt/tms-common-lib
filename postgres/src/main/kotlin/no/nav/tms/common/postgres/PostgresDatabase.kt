a07732762a1e0dad439d3a7cc8d7f32c6b93a9ed
Christopher Santana <christopher.santana@nav.no>
2026-01-09 15:29:49 +0100

    Legger tilbake Session som argument til queryBuilder så en kan bruke det bestemte typer spørringer.

--- postgres/src/main/kotlin/no/nav/tms/common/postgres/PostgresDatabase.kt
+++ postgres/src/main/kotlin/no/nav/tms/common/postgres/PostgresDatabase.kt
@@ -2,6 +2,7 @@ package no.nav.tms.common.postgres
 
 import com.zaxxer.hikari.HikariDataSource
 import kotliquery.Query
+import kotliquery.Session
 import kotliquery.action.ListResultQueryAction
 import kotliquery.action.NullableResultQueryAction
 
@@ -13,10 +14,12 @@ import java.sql.SQLException
 class PostgresDatabase internal constructor(
     val dataSource: HikariDataSource
 ) {
-    fun update(queryBuilder: () -> Query): Int {
+    fun update(queryBuilder: (Session) -> Query): Int {
         return try {
-            using(sessionOf(dataSource)) {
-                it.run(queryBuilder().asUpdate)
+            using(sessionOf(dataSource)) { session ->
+                queryBuilder(session)
+                    .asUpdate
+                    .let(session::run)
             }
         } catch (e: SQLException) {
             if (e.sqlState == PSQLState.UNIQUE_VIOLATION.state) {
@@ -27,21 +30,24 @@ class PostgresDatabase internal constructor(
         }
     }
 
-    fun <T> single(action: () -> NullableResultQueryAction<T>): T {
-        return using(sessionOf(dataSource)) {
-            it.run(action())
+    fun <T> single(queryBuilder: (Session) -> NullableResultQueryAction<T>): T {
+        return using(sessionOf(dataSource)) { session ->
+            queryBuilder(session)
+                .let(session::run)
         } ?: throw EmptyResultException()
     }
 
-    fun <T> singleOrNull(action: () -> NullableResultQueryAction<T>): T? {
-        return using(sessionOf(dataSource)) {
-            it.run(action())
+    fun <T> singleOrNull(queryBuilder: (Session) -> NullableResultQueryAction<T>): T? {
+        return using(sessionOf(dataSource)) { session ->
+            queryBuilder(session)
+                .let(session::run)
         }
     }
 
-    fun <T> list(action: () -> ListResultQueryAction<T>): List<T> {
-        return using(sessionOf(dataSource)) {
-            it.run(action())
+    fun <T> list(queryBuilder: (Session) -> ListResultQueryAction<T>): List<T> {
+        return using(sessionOf(dataSource)) { session ->
+            queryBuilder(session)
+                .let(session::run)
         }
     }
 
